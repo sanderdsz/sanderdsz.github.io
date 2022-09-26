1b3e0273303cbf6cfdcfe4a5534a72d9dc689e0e
Sander <sanderdsz@gmail.com>
2022-09-26 09:03:58 -0300

    resume update

--- pages/curriculum/assets/js/main.js
+++ pages/curriculum/assets/js/main.js
@@ -24,24 +24,28 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
 /*==================== SHOW SCROLL TOP ====================*/
 function scrollTop() {
@@ -90,42 +94,41 @@ themeButton.addEventListener("click", () => {
 
 /*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
 function scaleCv() {
-  document.body.classList.add('scale-cv')
+  document.body.classList.add("scale-cv");
 }
 
 /*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
 function removeScale() {
-  document.body.classList.remove('scale-cv')
+  document.body.classList.remove("scale-cv");
 }
 
 /*==================== GENERATE PDF ====================*/
 // PDF generated area
-let areaCv= document.getElementById('area-cv')
+let areaCv = document.getElementById("area-cv");
 
-let resumeButton = document.getElementById('resume-button')
+let resumeButton = document.getElementById("resume-button");
 
 // Html2pdf options
 let opt = {
-  margin:       0,
-  filename:     'sander-resume.pdf',
-  image:        { type: 'jpeg', quality: 0.98 },
-  html2canvas:  { scale: 4 },
-  jsPDF:        { format: 'a4', orientation: 'portrait' }
-}
+  margin: 0,
+  filename: "sander-resume.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: "a4", orientation: "portrait" },
+};
 
 // Function to call areaCv and Html2Pdf options
 function generateResume() {
-  html2pdf(areaCv, opt)
+  html2pdf(areaCv, opt);
 }
 
 // When the button is clicked, it executes the three functions
-resumeButton.addEventListener('click', () => {
+resumeButton.addEventListener("click", () => {
   // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-  scaleCv()
+  scaleCv();
   // 2. The PDF is generated
-  generateResume()
+  generateResume();
 
   // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
   // setTimeout(removeScale, 5000)
-})
-
+});
